53a381f6cc4ebcd63378ae8cd657e24cab0a4e2c
Shaun <shaun@thenetninja.co.uk>
2021-05-12 11:34:56 +0100

    lesson-4

--- main.go
+++ main.go
@@ -3,41 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	age := 35
+	name := "shaun"
 
-	// string variables
-	var nameOne string = "mario"
-	var nameTwo = "luigi"
-	var nameThree string
+	// Print
+	fmt.Print("hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("new line \n")
 
-	fmt.Print(nameOne, nameTwo, nameThree)
+	// Println
+	fmt.Println("hello ninjas!")
+	fmt.Println("goodbye ninjas!")
+	fmt.Println("my age is", age, "and my name is", name)
 
-	nameOne = "peach"
-	nameThree = "bowser"
+	// Printf (formatted string), %_ = format specifier
+	fmt.Printf("my age is %v and my name is %v \n", age, name) // %v = value in default format
+	fmt.Printf("my age is %q and my name is %q \n", age, name) // %q = quotes
+	fmt.Printf("age is of type %T \n", age)                    // %T is the type
+	fmt.Printf("you scored %f points! \n", 225.55)             // %f = float format
+	fmt.Printf("you scored %0.1f points! \n", 225.55)          // %0.2f = float with 2 decimal points
 
-	fmt.Print(nameOne, nameTwo, nameThree)
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is:", str)
 
-	// the following is allowed inside functions only
-	nameFour := "yoshi"
-	fmt.Print(nameFour)
-
-	// int variables
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Print(ageOne, ageTwo, ageThree)
-
-	// bits & memory
-	// var numOne int8 = 25
-	// var numTwo int8 = 128 // too large a number for 8-bit
-	// var numTwo uint = -25 unsigned ints cannot be negative
-
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 1965385877.5
-	var scoreThree = 1.5 // inferred as float64
-
-	fmt.Print(scoreOne, scoreTwo, scoreThree)
-
-	// for more info see https://golang.org/ref/spec#Numeric_types
+	// see more format specifiers here:
+	// https://golang.org/pkg/fmt/
 
 }
