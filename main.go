664f9b050d69b0a92db2d15ced31d9dd5c3d9a2e
Shaun <shaun@thenetninja.co.uk>
2021-05-12 12:28:13 +0100

    lesson-12

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
-// cannot use shorthand outside of functions
-// scoreTwo := 50
-
 func main() {
-	sayHello("mario")
-	showScore()
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
 	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		984759373: "luigi",
+		845775485: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[845775485])
+
+	phonebook[984759373] = "bowser"
+	fmt.Println(phonebook)
+
+	phonebook[647583927] = "yoshi"
+	fmt.Println(phonebook)
 }
