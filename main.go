5aba890c3a90925d7b130d382a6f0ad86837e572
Shaun <shaun@thenetninja.co.uk>
2021-05-12 12:51:54 +0100

    lesson-14

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-// func updateName(x string) {
-// 	x = "yoshi"
+// func updateName(n string) {
+// 	n = "wedge"
 // }
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateName(n *string) {
+	*n = "wedge"
 }
 
 func main() {
@@ -20,16 +15,17 @@ func main() {
 	name := "tifa"
 
 	// updateName(name)
-	name = updateName(name)
+	// fmt.Println(name)
 
+	// & gets the memory address of the value (pointer)
+	fmt.Println("memory address of name is:", &name)
+
+	// * gets the value at the specified memory address
+	m := &name
+	fmt.Println("memory address:", m)
+	fmt.Println("value at memory address:", *m)
+
+	updateName(&name) // using pointer as arg
 	fmt.Println(name)
 
-	// reference types -> slices, maps, functions, pointers
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 }
