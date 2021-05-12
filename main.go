f6f8a6da0f7efc34bd56e2f2bc6a60aa606ab2a9
Shaun <shaun@thenetninja.co.uk>
2021-05-12 17:29:22 +0100

    lesson-19

--- main.go
+++ main.go
@@ -17,9 +17,6 @@ func getInput(prompt string, r *bufio.Reader) (string, error) {
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	// fmt.Print("Create a new bill name: ")
-	// name, _ := reader.ReadString('\n')
-	// name = strings.TrimSpace(name)
 	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
@@ -31,8 +28,24 @@ func createBill() bill {
 func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
-	opt, _ := getInput("Choose option (a -add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		fmt.Println(tip)
+	case "s":
+		fmt.Println("you chose to save the bill")
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
 }
 
 func main() {
