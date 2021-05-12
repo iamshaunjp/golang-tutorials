263a73b59a713fd19d633eb966772901c9f5b324
Shaun <shaun@thenetninja.co.uk>
2021-05-12 14:44:33 +0100

    lesson-15

---
+++ bill.go
@@ -0,0 +1,17 @@
+package main
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
--- main.go
+++ main.go
@@ -2,30 +2,8 @@ package main
 
 import "fmt"
 
-// func updateName(n string) {
-// 	n = "wedge"
-// }
-
-func updateName(n *string) {
-	*n = "wedge"
-}
-
 func main() {
-	// value types -> strings, ints, bools, floats, arrays, structs
-	name := "tifa"
+	mybill := newBill("mario's bill")
 
-	// updateName(name)
-	// fmt.Println(name)
-
-	// & gets the memory address of the value (pointer)
-	fmt.Println("memory address of name is:", &name)
-
-	// * gets the value at the specified memory address
-	m := &name
-	fmt.Println("memory address:", m)
-	fmt.Println("value at memory address:", *m)
-
-	updateName(&name) // using pointer as arg
-	fmt.Println(name)
-
+	fmt.Println(mybill)
 }
