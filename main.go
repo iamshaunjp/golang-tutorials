f5dba23ac5ab3ed8b43e00ff416b0f6517c7d3c6
Shaun <shaun@thenetninja.co.uk>
2021-05-12 11:26:47 +0100

    lesson-3

--- main.go
+++ main.go
@@ -4,6 +4,40 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, ninjas!")
+	// string variables
+	var nameOne string = "mario"
+	var nameTwo = "luigi"
+	var nameThree string
+
+	fmt.Print(nameOne, nameTwo, nameThree)
+
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	fmt.Print(nameOne, nameTwo, nameThree)
+
+	// the following is allowed inside functions only
+	nameFour := "yoshi"
+	fmt.Print(nameFour)
+
+	// int variables
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Print(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var numOne int8 = 25
+	// var numTwo int8 = 128 // too large a number for 8-bit
+	// var numTwo uint = -25 unsigned ints cannot be negative
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 1965385877.5
+	var scoreThree = 1.5 // inferred as float64
+
+	fmt.Print(scoreOne, scoreTwo, scoreThree)
+
+	// for more info see https://golang.org/ref/spec#Numeric_types
 
 }
