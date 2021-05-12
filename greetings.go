0a4c88d83c97d7eba62e99fba26d20bdcb2e53cc
Shaun <shaun@thenetninja.co.uk>
2021-05-12 12:26:23 +0100

    lesson-11

---
+++ greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayHello(n string) {
+	fmt.Println("hello", n)
+}
+
+func showScore() {
+	fmt.Println("You scored this many points:", score)
+}
--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
+var score = 99.5
 
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+// cannot use shorthand outside of functions
+// scoreTwo := 50
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
+	sayHello("mario")
+	showScore()
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
-
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
