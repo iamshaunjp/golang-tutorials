47b9d466b7aaa4aed70da6a8d4e2fa0d37061245
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2021-05-14 17:12:16 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,5 @@ import "fmt"
 func main() {
 	mybill := newBill("mario's bill")
 
-	mybill.addItem("onion soup", 4.50)
-	mybill.addItem("veg pie", 8.95)
-	mybill.addItem("toffee pudding", 4.95)
-	mybill.addItem("coffee", 3.25)
-
 	fmt.Println(mybill.format())
-
 }
